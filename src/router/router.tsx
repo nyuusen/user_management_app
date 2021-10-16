401fc9ed07539f1a43939cb5cbb156296396a2d6
Irikawa Tomoyuki <t.irikawa@adseed.co.jp>
2021-10-16 17:51:15 +0900

    add: ルーティングを追加

---
+++ src/router/router.tsx
@@ -0,0 +1,36 @@
+import { VFC, memo } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import { Login } from 'components/pages/Login';
+import { homeRoutes } from './homeRoutes';
+import { Page404 } from 'components/pages/Page404';
+import { HeaderLayout } from 'components/templates/HeaderLayout';
+
+export const Router: VFC = memo(() => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Login></Login>
+      </Route>
+      <Route
+        path="/home"
+        render={({ match: { url } }) => (
+          <Switch>
+            {homeRoutes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                <HeaderLayout>{route.children}</HeaderLayout>
+              </Route>
+            ))}
+          </Switch>
+        )}
+      ></Route>
+      <Route path="*">
+        <Page404 />
+      </Route>
+    </Switch>
+  );
+});
